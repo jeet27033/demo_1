9d6d6ed4502b2cce33806397123de84e07a21f4e
Jeet Patel <jeet.patel@capillarytech.com>
2025-07-27 18:31:29 +0530

    Create javaTest.java

---
+++ javaTest.java
@@ -0,0 +1,7 @@
+public class BrokenExample {
+    public static void main(String[] args) {
+        System.out.println("This line is fine.");
+        int number = 10
+        System.out.println("Number is: " + number);
+    }
+}
